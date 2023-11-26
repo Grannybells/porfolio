7776ac0d0a919851bb08f15c0b819d00fd3135d3
Grannybells <zulueta.gran.p@gmail.com>
2023-11-26 23:45:13 +0800

    Project Nutrigrow

--- src/screen/Projects.jsx
+++ src/screen/Projects.jsx
@@ -14,6 +14,7 @@ import reactIco from "../assets/React.png"
 import cplusplus from "../assets/C++.png"
 import arduino from "../assets/Arduino.png"
 import firebase from "../assets/firebase.png"
+import eye from "../assets/Eye.svg"
 
 const Projects = () => {
     return (
@@ -24,7 +25,7 @@ const Projects = () => {
             <div className="flex items-center justify-center">
                 <div className="w-full flex flex-col items-center gap-10">
                     <h1 className="font-poppins text-[#10FFE6] text-4xl font-bold">Works</h1>
-                    <div className="w-full flex flex-row">
+                    {/* <div className="w-full flex flex-row">
                         <div className="w-full md:w-1/2 flex items-center justify-end">
                             <img className="h-48 md:h-96" src={project1} alt="github" />
                         </div>
@@ -39,11 +40,11 @@ const Projects = () => {
                             <p className="font-poppins text-sm text-white/80 text-justify h-64 lg:h-auto overflow-hidden">The web-based Inventory and Stock Management System streamlines inventory control by enabling users to track expiration dates and manage product quantities efficiently. It offers flexibility by allowing users to customize expiration dates for individual products, ensuring accurate monitoring of freshness. The system simplifies inventory updates and removal of expired items while maintaining precise stock counts through a dropdown menu for inputting product quantities.</p>
                             <div className="flex flex-row gap-5">
                                 <a href='https://github.com/Grannybells/x-track-web.git' className=" cursor-pointer">
-                                    <img href='https://github.com/Grannybells/x-track-web.git' className="h-10" src={github} alt="github" />
+                                    <img href='https://github.com/Grannybells/x-track-web.git' className="h-10 transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none" src={github} alt="github" />
                                 </a>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
                     <div className="w-full flex flex-row">
 
@@ -56,9 +57,17 @@ const Projects = () => {
                                 <img className="h-10 transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none" src={firebase} alt="logo" />
                             </div>
                             <p className="font-poppins text-sm text-white/80 text-justify h-64 lg:h-auto overflow-hidden">In the Philippines, the manual spreading of granular fertilizers in fields poses inefficiencies due to imprecise nutrient dispersal rates. To revolutionize agriculture, there's a critical need for an automated nutrient management system that operates in real time. This project aims to empower farmers with tools for effective crop nutrient management, reducing costs and optimizing planting processes by analyzing soil data and recommending ideal nutrient ratios for individual plants. This innovative system, boasting a success rate of 99.35% in various tests, promises to enhance livelihoods by automating planting procedures and boosting crop yields for high-value crops.</p>
-                            <a href='https://github.com/Grannybells/nutrigrow-app.git' className=" cursor-pointer">
-                                <img href='https://github.com/Grannybells/x-track-web.git' className="h-10" src={github} alt="github" />
-                            </a>
+                            <div className="flex flex-row gap-5">
+                                <a href='https://github.com/Grannybells/nutrigrow-app.git' className="gap-1 text-sm cursor-pointer flex items-center justify-center flex-col text-[#10FFE6]/50">
+                                    <img href='https://github.com/Grannybells/x-track-web.git' className="h-10 transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none" src={github} alt="github" />
+                                    Github
+                                </a>
+
+                                <a href='https://www.figma.com/proto/qxr2qOUPtMyGIjQu3VZzCY/Nutrigrow?type=design&node-id=1-6&t=s5qsXiyprblIHJoX-1&scaling=scale-down&page-id=0%3A1&mode=design' className="gap-1 text-sm cursor-pointer flex items-center justify-center flex-col text-[#10FFE6]/50">
+                                    <img href='https://github.com/Grannybells/x-track-web.git' className="h-10 transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none" src={eye} alt="github" />
+                                    Design
+                                </a>
+                            </div>
                         </div>
 
                         <div className="w-full md:w-1/2 flex justify-start items-center">
