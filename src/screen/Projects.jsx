db976f341ec6f7659c6dc3891ad1e9b2c0db0f0a
Gran Zulueta <zulueta.gran.p@gmail.com>
2023-12-21 09:30:48 +0800

    Update Projects.jsx

--- src/screen/Projects.jsx
+++ src/screen/Projects.jsx
@@ -25,39 +25,7 @@ const Projects = () => {
             <div className="flex items-center justify-center">
                 <div className="w-full flex flex-col items-center gap-10">
                     <h1 className="font-poppins text-[#10FFE6] text-4xl font-bold">Works</h1>
-                    <div className="w-full flex flex-row">
-                        <div className="w-full md:w-1/2 flex items-center justify-end">
-                            <img className="h-48 md:h-96" src={project1} alt="github" />
-                        </div>
-                        <div className="w-full md:w-1/2 p-5 flex flex-col gap-5 items-start">
-                            <h1 className="font-poppins text-2xl text-[#10FFE6] font-bold">X-Track</h1>
-                            <div className="border-[1px] w-48" />
-                            <div className="flex flex-row gap-5">
-                                <img className="h-10 transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none" src={reactIco} alt="logo" />
-                                <img className="h-10 transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none" src={tailwind} alt="logo" />
-                                <img className="h-10 transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none" src={firebase} alt="logo" />
-                            </div>
-                            <p className="font-poppins text-sm text-white/80 text-justify h-64 lg:h-auto overflow-hidden">
-                                Manage inventory effortlessly with our web-based system. Track expiration dates, customize them for freshness, update stock levels with ease, and remove expired items efficiently—all through user-friendly features like dropdown menus.
-                            </p>
-                            <div className="flex flex-row gap-5">
-                                <a href='https://github.com/Grannybells/x-track-web.git' className="gap-1 text-sm cursor-pointer flex items-center justify-center flex-col text-[#10FFE6]/50">
-                                    <img className="h-10 transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none" src={github} alt="github" />
-                                    Github
-                                </a>
-
-                                <a href='https://www.figma.com/proto/kQY7ogMlb7j82NBFlBN2Tq/X-Track?type=design&node-id=1-2&t=3uXTaE1VMz29qVd0-0&scaling=min-zoom&page-id=0%3A1' className="gap-1 text-sm cursor-pointer flex items-center justify-center flex-col text-[#10FFE6]/50">
-                                    <img className="h-10 transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none" src={eye} alt="github" />
-                                    Design
-                                </a>
-
-                                <a href='https://grannybells.github.io/x-track-web/' className="gap-1 text-sm cursor-pointer flex items-center justify-center flex-col text-[#10FFE6]/50">
-                                    <img className="h-10 transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none" src={eye} alt="github" />
-                                    Deployment
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                    
 
                     <div className="w-full flex flex-row">
 
